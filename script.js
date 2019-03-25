1ecc3deb1cdc8fa7951d971e143ffd5e62c772e7
zeduvo <flipo.morgann@gmail.com>
2019-03-25 16:17:25 +0100

    p

---
+++ script.js
@@ -0,0 +1,12 @@
+function burgerMobile(){
+   var menuBars = document.getElementById("menu");
+
+    if (menuBars.style.visibility == "initial"){
+        menuBars.style.visibility = "hidden";
+    }else{
+        menuBars.style.visibility = "initial";
+    }
+}
+function menuMovible(){
+    var movible = document.getElementById("MOVE");
+}
